31caa814b475a109092993b7fe08e1a6d4148e97
Andrew Crist <andrew.l.crist@gmail.com>
2017-01-28 16:57:18 -0700

    Add webpack
    Images weren't loading and I decided to go down the long, potentially mess, but thankfully quick and painless path of adding webpack and reconfiguring all paths

--- public/src/components/PinnedWeather.js
+++ public/src/components/PinnedWeather.js
@@ -4,6 +4,8 @@ import { Link } from 'react-router';
 const PinnedWeather = ({ state, id }) =>
 {
   let sp = ' '
+  let image = "../public/styles/images/1.png"
+  // let cityArray = [img + '1.png',img + '2.png',img + '3.png',img + '4.png',img + '5.png',img + '6.png',img + '7.png',img + '8.png',]
   return (
     <div className="pinned-weather">
       {state[id] ?
@@ -20,6 +22,7 @@ const PinnedWeather = ({ state, id }) =>
         </Link>
         :
         <Link className="a-pinned" to="/settings">
+          <img src={image} width="75px"/>
           Save a New City
         </Link>
       }
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    main: './public/src/index.js',
+  },
+  output: {
+    path: __dirname,
+    filename: '[name].bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react'],
+        },
+      },
+      {
+        test: /\.mp4$/,
+        loader: 'file',
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: [
+          'file?hash=sha512&digest=hex&name=[hash].[ext]',
+          'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false',
+        ],
+      },
+      { test: /\.css$/, loader: 'style!css' },
+      { test: /\.scss$/, loader: 'style!css!sass' },
+    ],
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['', '.js', '.jsx', '.json', '.scss', '.css'],
+  },
+};
