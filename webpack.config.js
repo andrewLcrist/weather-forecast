95abc057cc5e4ea4b779c1a9cbe6bf2779def080
Andrew Crist <andrew.l.crist@gmail.com>
2017-02-13 15:57:10 -0700

    Deploy

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   entry: {
-    main: './public/src/index.js',
+    main: ['babel-polyfill', './public/src/index.js']
   },
   output: {
     path: __dirname,
